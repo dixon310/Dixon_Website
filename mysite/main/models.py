1f2ace1b7efed190abf8c7f1f877419ac151b884
dixon310 <dixonchiu22@hotmail.com>
2020-12-10 19:30:18 +1100

    add page v1, Run migrate

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class ToDoList(models.Model):
+    name = models.CharField(max_Length=200)
+
+    def __str__(self):
+        return self.NAME
+
+class Item(models.Model):
+    ToDoList = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    text = models.ChaarField(max_Length=300)
+    complete = models.BooleanField()
+
+    def __str__(self):
+    return self.text
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(response):
     return HttpResponse("<h1>Welcome to Dixon page 1</h1>")
+
+def V1(response):
+    return HttpResponse("<h1>view 1</h1>")    
