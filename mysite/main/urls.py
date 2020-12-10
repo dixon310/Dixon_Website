f0d29ac09b2dacab477308de9669d6ffa560a364
dixon310 <dixonchiu22@hotmail.com>
2020-12-10 19:50:59 +1100

    Update

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("<int:id>", views.index, name="index"),
     path("v1/", views.index, name="view 1"),
     ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Welcome to Dixon page 1</h1>")
+def index(response, id):
+    return HttpResponse("<h1>Welcome to Dixon page 1</h1>" % id)
 
 def V1(response):
-    return HttpResponse("<h1>view 1</h1>")    
+    return HttpResponse("<h1>view 1</h1>")
