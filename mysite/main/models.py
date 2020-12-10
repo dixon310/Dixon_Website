90c1628cf356583444be114a5e8a8f5df11b4369
dixon310 <dixonchiu22@hotmail.com>
2020-12-10 19:36:58 +1100

    Update models.py

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 # Create your models here.
 class ToDoList(models.Model):
-    name = models.CharField(max_Length=200)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+
 class Item(models.Model):
     ToDoList = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
-    text = models.ChaarField(max_Length=300)
+    text = models.CharField(max_length=300)
     complete = models.BooleanField()
 
     def __str__(self):
