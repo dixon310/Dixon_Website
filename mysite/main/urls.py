7daca11cda0571c0ba8503687e9f024463c067b8
dixon310 <dixonchiu22@hotmail.com>
2020-12-14 13:42:59 +1100

    add create page

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("<int:id>", views.index, name="index"),
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("create/", views.create, name="create"),
     ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -9,3 +9,6 @@ def index(response, id):
 
 def home(response):
     return render(response, "main/home.html", {})
+
+def create(response):
+    return render(response, "main/home.html", {})
