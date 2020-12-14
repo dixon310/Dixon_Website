d40e510102b604014fe08779f19bad708dcc2b26
dixon310 <dixonchiu22@hotmail.com>
2020-12-14 11:42:37 +1100

    update urls.py

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:name>", views.index, name="index"),
+    path("<int:id>", views.index, name="index"),
     path("", views.home, name="home"),
     ]
