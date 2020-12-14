ff4102ef5489dff3357d3df02c49535d08055056
dixon310 <dixonchiu22@hotmail.com>
2020-12-14 12:30:50 +1100

    Update

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,7 +5,7 @@ from .models import ToDoList, Item
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    return render(response, "main/base.html", {})
+    return render(response, "main/list.html", {"ls":ls})
 
 def home(response):
     return render(response, "main/home.html", {})
