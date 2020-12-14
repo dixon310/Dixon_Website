bdfd046a2e04adb928875deb6430a25e08481cb6
dixon310 <dixonchiu22@hotmail.com>
2020-12-14 17:29:44 +1100

    Update

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
 from .forms import CreateNewList
 # Create your views here.
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
+
+    if response.method == 'POST':
+
     return render(response, "main/list.html", {"ls":ls})
 
 def home(response):
     return render(response, "main/home.html", {})
 
 def create(response):
-    form = CreateNewList()
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        if  form.is_valid():
+            n = form.cleaned_date["name"]
+            t = ToDoList(name=n)
+            t.save()
+
+        return HttpResponseRedirect("/%i" %t.id)
+
+    else:
+        form = CreateNewList()
     return render(response, "main/create.html", {"form":form})
