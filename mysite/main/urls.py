f45a47444e71d9f42013a7cef86f46115ef7e054
dixon310 <dixonchiu22@hotmail.com>
2020-12-14 22:20:00 +1100

    Add register, CSS

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("<int:id>", views.index, name="index"),
-    path("", views.home, name="home"),
+    path("home", views.home, name="home"),
     path("create/", views.create, name="create"),
     ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,7 +7,25 @@ from .forms import CreateNewList
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
 
+    #{"save":["save"], "c1":["clicked"]}
     if response.method == 'POST':
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+        if len(txt) > 2:
+            ls.item_set.create(text=txt, complete=False)
+        else:
+            print("invalid")
 
     return render(response, "main/list.html", {"ls":ls})
 
