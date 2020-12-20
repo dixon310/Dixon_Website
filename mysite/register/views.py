d6d22ef5d26008fa3b2e4f07db280511c0164452
dixon310 <dixonchiu22@hotmail.com>
2020-12-21 10:58:04 +1100

    add crispy, update

--- mysite/register/views.py
+++ mysite/register/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 
 
 # Create your views here.
 def register(response):
     if response.method == "POST":
-        form = UserCreationForm(response.POST)
+        form = RegistrationForm(response.POST)
         if form.is_valid():
             form.save()
     else:
-            form = UserCreationForm()
+            form = RegistrationForm()
     return render(response, "register/register.html", {"form":form})
